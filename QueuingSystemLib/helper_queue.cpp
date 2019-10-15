1e6f7130cfaa9004955710793a471bca672254b7
Aliaksei <alexx333by@protonmail.com>
2019-10-15 23:02:10 +0300

    Added customers queue

---
+++ QueuingSystemLib/helper_queue.cpp
@@ -0,0 +1,42 @@
+#include "helper_queue.h"
+
+using namespace queuing_system;
+
+helper_queue::helper_queue() : customers(), customers_mutex(), customer_condition_variable(), is_running(true)
+{ }
+
+void helper_queue::stop()
+{
+    if (is_running) {
+        is_running = false;
+        std::unique_lock<std::mutex> lock(customers_mutex);
+        customers.clear();
+        customer_condition_variable.notify_all();
+    }
+}
+
+void helper_queue::add_customer(const std::shared_ptr<customer> &customer)
+{
+    if (is_running) {
+        std::unique_lock<std::mutex> lock(customers_mutex);
+        customers.push_back(customer);
+        customer_condition_variable.notify_one();
+    }
+}
+
+std::shared_ptr<customer> helper_queue::get_customer()
+{
+    std::shared_ptr<customer> result;
+
+    if (is_running) {
+        std::unique_lock<std::mutex> lock(customers_mutex);
+        customer_condition_variable.wait(lock, [this] { return !is_running || !customers.empty(); });
+
+        if (is_running) {
+            result = customers.front();
+            customers.pop_front();
+        }
+    }
+
+    return result;
+}
---
+++ QueuingSystemLib/helper_queue.h
@@ -0,0 +1,30 @@
+#ifndef CONTINUOUSSTOCHASTICQUEUINGSYSTEM_HELPER_QUEUE_H
+#define CONTINUOUSSTOCHASTICQUEUINGSYSTEM_HELPER_QUEUE_H
+
+
+#include <memory>
+#include <deque>
+#include <mutex>
+#include <condition_variable>
+
+namespace queuing_system {
+    class customer;
+
+    class helper_queue {
+    public:
+        helper_queue();
+
+        void add_customer(const std::shared_ptr<customer> &customer);
+        std::shared_ptr<customer> get_customer();
+        void stop();
+
+    private:
+        std::deque<std::shared_ptr<customer>> customers;
+        std::mutex customers_mutex;
+        std::condition_variable customer_condition_variable;
+        bool is_running;
+    };
+}
+
+
+#endif //CONTINUOUSSTOCHASTICQUEUINGSYSTEM_HELPER_QUEUE_H
